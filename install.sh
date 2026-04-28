0368a8019f26665cef7e09a836f7a6d12558d98c
soapmishra <soapmishra@gmail.com>
2026-04-28 15:05:40 +0530

    Add customization options post install

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 echo installing required packages
-sudo pacman -S --needed $(cat ./pacman_packages)
+sudo pacman -S --noconfirm  $(cat ./pacman_packages)
 
 echo linking rc
 ln $PWD/rc $HOME/.rc_soap
@@ -9,6 +9,12 @@ ln $PWD/rc $HOME/.rc_soap
 echo activating .rc_soap in zshrc
 head -n -1 /usr/share/oh-my-zsh/zshrc | tee $HOME/.zshrc
 echo "source $HOME/.rc_soap" >> $HOME/.zshrc
+echo "# CUSTOMIZATION" >> $HOME/.zshrc
+echo "ZSH_THEME=\$ZSH_THEME" >> $HOME/.zshrc
+echo "plugins=(\$plugins)" >> $HOME/.zshrc
+
+echo >> $HOME/.zshrc
+echo "# OH-MY-ZSH CALL" >> $HOME/.zshrc
 tail -n 1 /usr/share/oh-my-zsh/zshrc | tee -a $HOME/.zshrc
 
 echo Changing default shell to zsh
